0333e4d0d517625f5367b909e6f33cf95af61899
Peter Jalbert <peterjalbert@Peters-MBP.domain_not_set.invalid>
2016-12-14 02:03:30 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+The Chipotle Finder
+
+This web application uses the Google Maps API and Javascript to locate the Chipotle 
+Mexican Grills around you for your convenience.
---
+++ chipotle.js
@@ -0,0 +1,181 @@
+//Chipotle Finder
+
+
+var map;
+var service;
+var marker;
+var pos;
+var infowindow;
+var cardCount = 1;
+var idArray = [];
+
+
+function initMap() {
+    /* Initializes the map and begins the application cycle.
+    */
+    var mapOptions = {
+        zoom: 14
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            pos = new google.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
+
+            infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'Found you!'
+            });
+
+            map.setCenter(pos);
+
+            var request = {
+                location:pos,
+                radius:5000,
+                keyword: ['Chipotle Mexican Grill']
+            };
+
+            infowindow = new google.maps.InfoWindow();
+            var service = new google.maps.places.PlacesService(map);
+            
+            //Searches for nearby Chipotle
+            service.nearbySearch(request,callback);
+        }, function() {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results);
+            
+            for (var i = 0; i < results.length; i++) {
+                if(results[i].name === "Chipotle Mexican Grill"){
+                //Keeps track of number of Cards
+                cardCount++;
+                    
+                //Adds marker to the map
+                createMarker(results[i]);
+                    
+                //Gets details about the Chipotle
+                var service2 = new google.maps.places.PlacesService(map);
+                service2.getDetails({
+                    placeId: results[i].place_id
+                }, function(place, status){
+                    //creates Card for Chipotle on page
+                    createCard(place);
+                });
+
+                }
+            }
+            if (cardCount === 1){
+                //If there are no Chipotles in range, alerts the user
+                noChipotle();
+            }
+        }
+    }
+}
+
+function noChipotle(){
+    /* In the event that no Chipotles are located in the search radius, 
+    this function adds an element to the page that alerts the user.
+    */
+    var element = document.getElementById("button_container");
+    var error = document.createElement("div");
+    error.className = "failed";
+    var errorText = document.createTextNode("No Chiptole Nearby...");
+    error.appendChild(errorText);
+    element.appendChild(error);
+}
+
+function createMarker(place) {
+    /* Given a place from the Google Places search results, this function
+    adds a marker to the main map, and labels it with the place name.
+    */
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: placeLoc
+        });
+        
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
+
+function createCard(place) {
+    /* This function creates a W3 card in the button container div on the page.
+    The card includes the name, address, phone number, rating, and a review. 
+    */
+    
+    //Creates the card 
+    var element = document.getElementById("button_container");
+    var newCard = document.createElement("div");
+    newCard.className = "w3-card-2";
+    newCard.setAttribute("id", "card" + cardCount);  
+    element.appendChild(newCard);
+    
+    //adds the Place name to the card
+    var name = document.createElement("p");
+    name.className = "place_title";
+    var nameText = document.createTextNode(place.name);
+    name.appendChild(nameText);
+    newCard.appendChild(name);
+    
+    //adds whether the location is currently open to the card
+    var hours = document.createElement("div");
+    hours.className = "open_hours";
+    var hoursText = document.createTextNode("Am I Open?:  " + openBoolean(place.opening_hours.open_now));
+    hours.appendChild(hoursText);
+    newCard.appendChild(hours);
+    
+    //adds the address to the card
+    var address = document.createElement("div");
+    address.className = "address";
+    var addressText = document.createTextNode("Address:  " + place.formatted_address)
+    address.appendChild(addressText);
+    newCard.appendChild(address);
+    
+    //adds the phone number to the card
+    var phoneNumber = document.createElement("div");
+    address.className = "phone_number";
+    var phoneNumberText = document.createTextNode("Phone Number:  " + place.formatted_phone_number);
+    phoneNumber.appendChild(phoneNumberText);
+    newCard.appendChild(phoneNumber);
+
+    //adds the rating to the card
+    var rating = document.createElement("div");
+    rating.className = "rating";
+    var ratingText = document.createTextNode("Rating: " + place.rating);
+    rating.appendChild(ratingText);
+    newCard.appendChild(rating);
+
+    //adds a review to the card
+    var review = document.createElement("p");
+    review.className = "review";
+    var reviewText = document.createTextNode('"' + place.reviews[0].text + '"');
+    review.appendChild(reviewText);
+    newCard.appendChild(review);
+    console.log("created Card");
+    
+}
+
+function openBoolean(bool) {
+    /* This function takes a boolean that is retrieved from the place Details request.
+    Based on the value of the boolean, it returns a sting to be added to the createCard function.
+    */
+    if (bool === true) {
+        return "Open for business!";
+    }
+    else {
+        return "Maybe another time! :("
+    }
+}
